08ae7c7dcc8fe414824bb948d1007ba847144535
sdannyandres <sdannyandres@gmail.com>
2022-11-19 21:20:38 +0100

    react-typescript-query

--- src/main.tsx
+++ src/main.tsx
@@ -1,84 +1,26 @@
 import ReactDom from 'react-dom/client';
-import { useState, useEffect,useCallback, memo } from 'react';
-interface IRegistro {
-  id: string;
-  nombre: string
+import { QueryClientProvider, QueryClient, useQuery } from 'react-query'
+const root = document.getElementById('root') as HTMLElement
+
+interface IAppProps {
+  id:number
 }
 
-var root = document.getElementById('root') as HTMLElement
-
-interface IItemProps {
-  item: IRegistro
-  deleteRegistro: (id: string) => void
-}
-const Item: React.FC<IItemProps> = memo(({ item, deleteRegistro}) =>{
-useEffect(() => {
-  console.log('item', item)
-})
-
-return <li>{ item.id } { item.nombre } <button onClick={() => deleteRegistro(item.id)}>delete</button></li> 
-})
-
-
-interface IListaProps {
-  registros: IRegistro[]
-  deleteRegistro: (id: string) => void
-}
-
-const Lista: React.FC<IListaProps> = memo(({ deleteRegistro,registros }) => {
-  useEffect(()=> {
-    console.log('lista')
+const App: React.FC<IAppProps> = ({id}) => {
+  const {isLoading, data} = useQuery(["post"], async () => {
+    const datos = await (await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)).json()
+    return datos
   })
-  return <ul>
-    {
-      registros.map((item: IRegistro, index: number) =>
-        <Item key ={ index } deleteRegistro={deleteRegistro} item = { item } > </Item>
-      )
-    }
-    < /ul>
-})
 
-const valoresIniciales: IRegistro[] = [
-  {
-    id: "1",
-
-    nombre: "producto 2",
-  },
-  {
-    id: "2",
-
-    nombre:"producto 2",
-  }
-]
-const App = () => {
-  const [texto, setTexto] = useState("")
-  const [productos,setProductos] = useState<IRegistro[]>(valoresIniciales)
-  useEffect(() => {
-    console.log('app')
-})
-const addRegistro = () => {
-  const nuevo: IRegistro = {
-    id: new Date().getTime().toString(),
-    nombre: texto
-  }
-  setProductos([...productos, nuevo])
-  
- 
+if (isLoading) return <p>Esta cargando</p>
+return <div> {JSON.stringify(data)} </div>
 }
-const deleteRegistro= useCallback((id: string) => {
-  setProductos (productos.filter(item => item.id != id))
-},[productos])
 
-return <div>
-  <input type="text" value={texto} onChange={ (e) => setTexto(e.target.value)
-    } />
-    <button onClick={ () => addRegistro() }>ADD</button>
-    <Lista registros={productos} deleteRegistro={deleteRegistro}/>
-    </div>
-    }
+const queryClient = new QueryClient()
 
 ReactDom.createRoot(root).render(
-  <App />
-)
-    
- 
+  <QueryClientProvider client={queryClient}>
+    <App id={10} />
+    </QueryClientProvider>
+
+  )
